d06e7e7c038df6c61c95e31662621f0ce563ef18
serca <serca@DESKTOP-MCID6LJ>
2023-03-20 00:16:43 +0300

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Kisiler.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <label for="TextBoxAd">Ad:</label>
+            <asp:TextBox ID="TextBoxAd" runat="server"></asp:TextBox>
+            <br />
+            <label for="TextBoxSoyad">Soyad:</label>
+            <asp:TextBox ID="TextBoxSoyad" runat="server"></asp:TextBox>
+            <br />
+            <label for="TextBoxTelefonNo">Telefon No:</label>
+            <asp:TextBox ID="TextBoxTelefonNo" runat="server"></asp:TextBox>
+            <br />
+            <label for="TextBoxTCKimlikNo">T.C. Kimlik No:</label>
+            <asp:TextBox ID="TextBoxTCKimlikNo" runat="server"></asp:TextBox>
+            <br />
+            <label for="TextBoxDogumTarihi">Doğum Tarihi:</label>
+            <asp:TextBox ID="TextBoxDogumTarihi" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="ButtonKaydet" runat="server" Text="Kaydet" OnClick="ButtonKaydet_Click" />
+            <br />
+            <br />
+            <asp:Label ID="SuccessLabel" runat="server" ForeColor="Green"></asp:Label>
+            <asp:Label ID="HataLabel" runat="server" ForeColor="Red"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,66 @@
+﻿using Kisiler.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Protocols;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Kisiler
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ButtonKaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ad = TextBoxAd.Text;
+                string soyad = TextBoxSoyad.Text;
+                string telefonNo = TextBoxTelefonNo.Text;
+                long tcKimlikNo = Convert.ToInt64(TextBoxTCKimlikNo.Text);
+                DateTime dogumTarihi = Convert.ToDateTime(TextBoxDogumTarihi.Text);
+
+                // NVİ'ne bağlanarak girilen bilgilerin gerçek bir vatandaşa ait olup olmadığını kontrol ettım
+                KPSPublicSoapClient client = new KPSPublicSoapClient();
+                bool result = client.TCKimlikNoDogrula(tcKimlikNo, ad.ToUpper(), soyad.ToUpper(), dogumTarihi.Year);
+
+                if (!result)
+                {
+                    HataLabel.Text = "Hata: Girilen bilgiler gerçek bir vatandaşa ait değil.";
+                    return;
+                }
+
+                // Veritabanına kaydedelim
+                string connectionString = "Server=91.93.1.160;Database=sergu;User Id=sergu;Password=SercanGure*1879;";
+                SqlConnection connection = new SqlConnection(connectionString);
+                connection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO Kisiler (Ad, Soyad, TelefonNo, TcKimlikNo, DogumTarihi, KayitTarihi) VALUES (@Ad, @Soyad, @TelefonNo, @TcKimlikNo, @DogumTarihi, @KayitTarihi)", connection);
+                command.Parameters.AddWithValue("@Ad", ad);
+                command.Parameters.AddWithValue("@Soyad", soyad);
+                command.Parameters.AddWithValue("@TelefonNo", telefonNo);
+                command.Parameters.AddWithValue("@TcKimlikNo", tcKimlikNo);
+                command.Parameters.AddWithValue("@DogumTarihi", dogumTarihi);
+                command.Parameters.AddWithValue("@KayitTarihi", DateTime.Now);
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                SuccessLabel.Text = "Kayıt işlemi başarıyla tamamlandı.";
+            }
+            catch (SoapException ex)
+            {
+                HataLabel.Text = "Hata: " + ex.Detail.InnerText;
+            }
+            catch (Exception ex)
+            {
+                HataLabel.Text = "Hata: " + ex.Message;
+            }
+        }
+    }
+}
